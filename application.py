cfc6b9e5df268b9f23a88ad95eb3405d05e06d33
Nicolas Kupfer <jones.godel@gmail.com>
2020-05-20 16:17:42 +0200

    Compile script added

--- application.py
+++ application.py
@@ -1,58 +1,75 @@
 import tkinter as tk
-import sip
-import PyQt4._qt
-# from PIL import Image, ImageTk
 from config import StatusColor, StatusMessage
 
+_is_mock_ = False
 
-class Application(tk.Frame):
-    _info_text: tk.StringVar
-    _status_text: tk.StringVar
-    _status_label: tk.Label
-    _info_label: tk.Label
-    _scayla_logo_label: tk.Label
-    _app_name: tk.Label
+if _is_mock_:
+    print('Using command-line only. No UI')
+    class Application():
+        _mock: str
 
-    def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
-        self.pack()
-        # self.socket = socket
-        self.createWidgets()
+        def setInfo(self, mock):
+            self._mock = mock
 
-    def createWidgets(self):
-        self._info_text = tk.StringVar()
-        self._status_text = tk.StringVar()
+        def setStatus(self, mock):
+            self._mock = mock
 
-        self._info_text.set("... ...")
-        self._status_text.set(StatusMessage().disconnected)
+        def setStatusColor(self, mock):
+            self._mock = mock
 
-        scayla_logo = tk.PhotoImage(file="scayla2.gif")
-        self._scayla_logo_label = tk.Label(self, image=scayla_logo)
-        self._scayla_logo_label.image = scayla_logo
-        self._scayla_logo_label.pack()
+        def mainloop(self):
+            self._mock = ""
 
-        self._app_name = tk.Label(self, text="Companion App", fg=StatusColor.blue, font=('helvetica', 25, 'bold'))
-        self._app_name.pack(padx=20, pady=0)
+else:
+    class Application(tk.Frame):
+        _info_text: tk.StringVar
+        _status_text: tk.StringVar
+        _status_label: tk.Label
+        _info_label: tk.Label
+        _scayla_logo_label: tk.Label
+        _app_name: tk.Label
 
-        self._status_label = tk.Label(self, textvariable=self._status_text, fg=StatusColor.blue,
-                                      font=('helvetica', 20, 'bold'))
-        self._status_label.pack(padx=20, pady=10)
-        #
-        self._info_label = tk.Label(self, textvariable=self._info_text, fg=StatusColor.lightBlue,
-                                    font=('helvetica', 15, 'bold'))
-        self._info_label.pack(padx=20, pady=10)
+        def __init__(self, master=None):
+            tk.Frame.__init__(self, master)
+            self.pack()
+            # self.socket = socket
+            self.createWidgets()
 
-    def closeApp(self):
-        self.quit()
+        def createWidgets(self):
+            self._info_text = tk.StringVar()
+            self._status_text = tk.StringVar()
 
-    def setInfo(self, text: StatusMessage):
-        self._info_text.set(text)
+            self._info_text.set("... ...")
+            self._status_text.set(StatusMessage().disconnected)
 
-    def setStatus(self, text: StatusMessage):
-        self._status_text.set(text)
+            scayla_logo = tk.PhotoImage(file="scayla2.gif")
+            self._scayla_logo_label = tk.Label(self, image=scayla_logo)
+            self._scayla_logo_label.image = scayla_logo
+            self._scayla_logo_label.pack()
 
-    def setStatusColor(self, color: StatusColor):
-        self._status_label.config(fg=color)
+            self._app_name = tk.Label(self, text="Companion App", fg=StatusColor.blue, font=('helvetica', 25, 'bold'))
+            self._app_name.pack(padx=20, pady=0)
+
+            self._status_label = tk.Label(self, textvariable=self._status_text, fg=StatusColor.blue,
+                                          font=('helvetica', 20, 'bold'))
+            self._status_label.pack(padx=20, pady=10)
+            #
+            self._info_label = tk.Label(self, textvariable=self._info_text, fg=StatusColor.lightBlue,
+                                        font=('helvetica', 15, 'bold'))
+            self._info_label.pack(padx=20, pady=10)
+
+        def closeApp(self):
+            self.quit()
+
+        def setInfo(self, text: StatusMessage):
+            self._info_text.set(text)
+
+        def setStatus(self, text: StatusMessage):
+            self._status_text.set(text)
+
+        def setStatusColor(self, color: StatusColor):
+            self._status_label.config(fg=color)
+
 
 
 application = Application()
